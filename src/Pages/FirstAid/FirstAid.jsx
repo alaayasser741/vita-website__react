c2278964a206a879e48843dd9b55dc7a7f798cb6
Alaa Yasser <alaayasser2018@gmail.com>
2023-04-18 22:49:19 +0200

    add first Aid Links

--- src/Pages/FirstAid/FirstAid.jsx
+++ src/Pages/FirstAid/FirstAid.jsx
@@ -23,19 +23,19 @@ const FirstAid = () => {
             </div>
             <ul className="FirstAid__Link">
               <li>
-                <Link to='/'>الاختناق</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D8%A5%D8%B3%D8%B9%D9%8E%D8%A7%D9%81%D8%A7%D8%AA%D9%8F-%D8%A7%D9%84%D8%A3%D9%88%D9%91%D9%8E%D9%84%D9%8A%D9%91%D9%8E%D8%A9/%D8%A7%D9%84%D8%A7%D8%AE%D8%AA%D9%86%D8%A7%D9%82-%D8%A3%D9%88-%D8%A7%D9%84%D8%B4%D9%91%D9%8E%D8%B1%D9%8E%D9%82'>الاختناق</Link>
               </li>
               <li>
-                <Link to='/'>إصابات البرق</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D8%A7%D9%84%D9%86%D8%A7%D8%AC%D9%85%D8%A9-%D8%B9%D9%86-%D8%A7%D9%84%D8%B5%D8%AF%D9%85%D8%A7%D8%AA-%D8%A7%D9%84%D9%83%D9%87%D8%B1%D8%A8%D8%A7%D8%A6%D9%8A%D8%A9-%D9%88%D8%A7%D9%84%D8%B5%D9%88%D8%A7%D8%B9%D9%82/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D8%A7%D9%84%D9%86%D8%A7%D8%AC%D9%85%D8%A9-%D8%B9%D9%86-%D8%A7%D9%84%D8%B5%D9%88%D8%A7%D8%B9%D9%82'>إصابات البرق</Link>
               </li>
               <li>
-                <Link to='/'>الإصابات الكهربائية</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D8%A7%D9%84%D9%86%D8%A7%D8%AC%D9%85%D8%A9-%D8%B9%D9%86-%D8%A7%D9%84%D8%B5%D8%AF%D9%85%D8%A7%D8%AA-%D8%A7%D9%84%D9%83%D9%87%D8%B1%D8%A8%D8%A7%D8%A6%D9%8A%D8%A9-%D9%88%D8%A7%D9%84%D8%B5%D9%88%D8%A7%D8%B9%D9%82/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D8%A7%D9%84%D9%86%D8%A7%D8%AC%D9%85%D8%A9-%D8%B9%D9%86-%D8%A7%D9%84%D8%B5%D8%AF%D9%85%D8%A7%D8%AA-%D8%A7%D9%84%D9%83%D9%87%D8%B1%D8%A8%D8%A7%D8%A6%D9%8A%D8%A9'>الإصابات الكهربائية</Link>
               </li>
               <li>
-                <Link to='/'>إنخفاض حرارة الجسم</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA%D9%8F-%D8%A7%D9%84%D8%A8%D8%B1%D8%AF/%D8%A7%D9%86%D8%AE%D9%81%D8%A7%D8%B6%D9%8F-%D8%AD%D9%8E%D8%B1%D8%A7%D8%B1%D9%8E%D8%A9-%D8%A7%D9%84%D8%AC%D8%B3%D9%85-hypothermia'>إنخفاض حرارة الجسم</Link>
               </li>
               <li>
-                <Link to='/'>جروح</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D8%A5%D8%B3%D8%B9%D9%8E%D8%A7%D9%81%D8%A7%D8%AA%D9%8F-%D8%A7%D9%84%D8%A3%D9%88%D9%91%D9%8E%D9%84%D9%8A%D9%91%D9%8E%D8%A9/%D8%A7%D9%84%D8%AC%D8%B1%D9%88%D8%AD'>جروح</Link>
               </li>
             </ul>
           </div>
@@ -45,19 +45,19 @@ const FirstAid = () => {
             </div>
             <ul className="FirstAid__Link">
               <li>
-                <Link to='/'>اولويات الاسعافات الاولية</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D8%A5%D8%B3%D8%B9%D9%8E%D8%A7%D9%81%D8%A7%D8%AA%D9%8F-%D8%A7%D9%84%D8%A3%D9%88%D9%91%D9%8E%D9%84%D9%8A%D9%91%D9%8E%D8%A9/%D8%A3%D9%88%D9%84%D9%88%D9%8A%D9%91%D9%8E%D8%A7%D8%AA-%D8%A7%D9%84%D8%A5%D8%B3%D8%B9%D8%A7%D9%81%D8%A7%D8%AA-%D8%A7%D9%84%D8%A3%D9%88%D9%91%D9%8E%D9%84%D9%8A%D8%A9-%D9%81%D9%8A-%D8%AD%D8%A7%D9%84%D8%A7%D8%AA-%D8%A7%D9%84%D8%B7%D9%88%D8%A7%D8%B1%D8%A6'>اولويات الاسعافات الاولية</Link>
               </li>
               <li>
-                <Link to='/'>حالات ثني والتواء</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D8%A5%D8%B3%D8%B9%D9%8E%D8%A7%D9%81%D8%A7%D8%AA%D9%8F-%D8%A7%D9%84%D8%A3%D9%88%D9%91%D9%8E%D9%84%D9%8A%D9%91%D9%8E%D8%A9/%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D8%A7%D9%84%D9%86%D9%91%D9%8E%D8%B3%D9%8A%D8%AC-%D8%A7%D9%84%D8%B1%D9%91%D9%8E%D8%AE%D9%88'>حالات ثني والتواء</Link>
               </li>
               <li>
-                <Link to='/'>حروق</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D8%AD%D9%8F%D8%B1%D9%8F%D9%88%D9%82%D9%8F/%D8%A7%D9%84%D8%AD%D9%8F%D8%B1%D9%88%D9%82'>حروق</Link>
               </li>
               <li>
-                <Link to='/'>حروق العين الكيميائية</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D8%A7%D9%84%D8%B9%D9%8A%D9%86/%D8%AD%D8%B1%D9%88%D9%82-%D9%81%D9%8A-%D8%A7%D9%84%D8%B9%D9%8A%D9%86'>حروق العين الكيميائية</Link>
               </li>
               <li>
-                <Link to='/'>داء المرتفعات</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A3%D9%85%D8%B1%D9%8E%D8%A7%D8%B6-%D8%A7%D9%84%D9%85%D9%8F%D8%B1%D8%AA%D9%8E%D9%81%D8%B9%D8%A7%D8%AA%D9%90/%D8%A3%D9%85%D8%B1%D8%A7%D8%B6-%D8%A7%D9%84%D9%85%D9%8F%D8%B1%D8%AA%D9%8E%D9%81%D8%B9%D8%A7%D8%AA'>داء المرتفعات</Link>
               </li>
             </ul>
           </div>
@@ -67,19 +67,19 @@ const FirstAid = () => {
             </div>
             <ul className="FirstAid__Link">
               <li>
-                <Link to='/'>توقف القلب</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D8%B6%D8%B7%D8%B1%D8%A7%D8%A8%D8%A7%D8%AA-%D8%A7%D9%84%D9%82%D9%84%D8%A8-%D9%88%D8%A7%D9%84%D8%A3%D9%88%D8%B9%D9%90%D9%8A%D8%A9-%D8%A7%D9%84%D8%AF%D9%91%D9%8E%D9%85%D9%88%D9%8A%D9%91%D9%8E%D8%A9/%D9%81%D8%B4%D9%84-%D8%A7%D9%84%D9%82%D9%84%D8%A8/%D9%81%D8%B4%D9%84-%D8%A7%D9%84%D9%82%D9%84%D8%A8?query=%D8%AA%D9%88%D9%82%D9%81%20%D8%A7%D9%84%D9%82%D9%84%D8%A8'>توقف القلب</Link>
               </li>
               <li>
-                <Link to='/'>عضََات البشر</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D9%84%D9%91%D9%8E%D8%AF%D8%BA%D9%8E%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D9%84%D8%B3%D9%91%D9%8E%D8%B9%D8%A7%D8%AA/%D8%B9%D8%B6%D9%91%D9%8E%D8%A7%D8%AA-%D8%A7%D9%84%D8%A8%D8%B4%D8%B1'>عضََات البشر</Link>
               </li>
               <li>
-                <Link to='/'>عضََات الحيوانات</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D9%84%D9%91%D9%8E%D8%AF%D8%BA%D9%8E%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D9%84%D8%B3%D9%91%D9%8E%D8%B9%D8%A7%D8%AA/%D8%B9%D8%B6%D9%91%D9%8E%D8%A7%D8%AA-%D8%A7%D9%84%D8%AD%D9%8A%D9%88%D8%A7%D9%86%D8%A7%D8%AA'>عضََات الحيوانات</Link>
               </li>
               <li>
-                <Link to='/'>قضمة الصقيع</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA%D9%8F-%D8%A7%D9%84%D8%A8%D8%B1%D8%AF/%D9%82%D9%8E%D8%B6%D9%85%D9%8E%D8%A9-%D8%A3%D9%88-%D8%B9%D8%B6%D9%91%D9%8E%D8%A9-%D8%A7%D9%84%D8%B5%D9%91%D9%8E%D9%82%D9%8A%D8%B9-frostbite'>قضمة الصقيع</Link>
               </li>
               <li>
-                <Link to='/'>لدغات أفعى</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D9%84%D9%91%D9%8E%D8%AF%D8%BA%D9%8E%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D9%84%D8%B3%D9%91%D9%8E%D8%B9%D8%A7%D8%AA/%D9%84%D8%AF%D8%BA%D8%A7%D8%AA-%D8%A7%D9%84%D8%A3%D9%81%D8%A7%D8%B9%D9%8A'>لدغات أفعى</Link>
               </li>
             </ul>
           </div>
@@ -89,10 +89,10 @@ const FirstAid = () => {
             </div>
             <ul className="FirstAid__Link">
               <li>
-                <Link to='/'>الغرق</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D8%BA%D9%8E%D8%B1%D9%8E%D9%82/%D8%BA%D9%8E%D8%B1%D9%8E%D9%82'>الغرق</Link>
               </li>
               <li>
-                <Link to='/'>لسعات النحل</Link>
+                <Link to='https://www.msdmanuals.com/ar/home/%D8%A7%D9%84%D8%A5%D8%B5%D8%A7%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D8%AA%D9%91%D9%8E%D8%B3%D9%85%D9%91%D9%8F%D9%85/%D8%A7%D9%84%D9%84%D9%91%D9%8E%D8%AF%D8%BA%D9%8E%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D9%84%D8%B3%D9%91%D9%8E%D8%B9%D8%A7%D8%AA/%D9%84%D8%B3%D8%B9%D8%A7%D8%AA-%D8%A7%D9%84%D9%86%D8%AD%D9%84-%D9%88%D8%A7%D9%84%D8%AF%D8%A8%D8%A7%D8%A8%D9%8A%D8%B1-%D9%88%D8%A7%D9%84%D8%B2%D9%86%D8%A7%D8%A8%D9%8A%D8%B1-%D9%88%D8%A7%D9%84%D9%86%D9%85%D9%84'>لسعات النحل</Link>
               </li>
             </ul>
           </div>
